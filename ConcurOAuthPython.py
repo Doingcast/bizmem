6b91312a9a4dd5a7d458669c103084c675e623b0
Fernando Freitas Alves <ffreitasalves@gmail.com>
2014-09-06 17:52:44 -0700

    Initial Commit

---
+++ ConcurOAuthPython.py
@@ -0,0 +1,38 @@
+#from https://gist.github.com/Trudeaucj/09c25e79c332e93703a0
+import requests
+import json
+import base64
+
+baseUrl = 'https://www.concursolutions.com'
+oauthUrl = baseUrl+'/net2/oauth2/accesstoken.ashx'
+reportDigestUrl = ''
+
+def getbasic(user, password):
+    # basic authentication (according to HTTP)
+    return base64.encodestring(user + ":" + password)
+
+
+def getTokenGivenUsernamePasswordAndConsumerKey(username, password, consumerKey):
+    """
+    Given:
+        String username (Concur username)
+        String password (Concur password)
+        String ConsumerKey (Registered partner applciation consumerKey)
+
+    Example:
+        Python dictionary of the Oauth Response:
+        getTokenGivenUsernamePasswordAndConsumerKey('example@concur.com', '12345', 'ConsumerKey')
+        {u'Access_Token': {u'Expiration_date': u'5/7/2015 10:17:14 PM', u'Token': u'NhYhZsP1zK1sARLHkWqSJpwfBIw=',
+        u'Instance_Url': u'https://www.concursolutions.com/', u'Refresh_Token': u's9Scoa2GdFkjQYFRgsXKrdcwJBrgIan'}}
+
+        You can then save this token and use it in the headers of your HTTP calls with the parameter:
+        Authorization: OAuth {Token}
+
+    """
+    basic = 'Basic ' + getbasic(username, password)
+    headers1 = {'Authorization': basic.rstrip(), 'X-ConsumerKey': consumerKey, 'Accept':'application/json'}
+    r = requests.get(oauthUrl, headers=headers1)
+    return json.loads(r.content)
+
+if __name__ == '__main__':
+    print getTokenGivenUsernamePasswordAndConsumerKey('user1@concurdisrupt.com', 'disrupt', '<Insert Concur consumer key here>')
---
+++ hackdisrupt.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import requests
+from requests_oauthlib import OAuth2, OAuth1
+import json
+from xml.etree import ElementTree
+from ConcurOAuthPython import getbasic, getTokenGivenUsernamePasswordAndConsumerKey
+
+#Constants
+USER = "user46@concurdisrupt.com"
+PWD = "disrupt"
+KEY = 'mbGLfEhU1rUUG8IOImdmVt'
+TOKEN = 'NPuB/Wr+a9OkYNWz9Mw/kWNWvyk='
+SAMPLE_REPORT_NAME = 'Disrupt Hackathon NYC Trip'
+SAMPLE_REPORT_ID = 'D7E30A5A1E1F4ACF82FE'
+
+#Initializing FLASK app
+app = Flask(__name__)
+
+#tests
+basic = 'Basic ' + getbasic(USER, PWD)
+headers1 = getTokenGivenUsernamePasswordAndConsumerKey('user46@concurdisrupt.com', 'disrupt', KEY)
+
+#oauth
+auth = {
+    'Authorization': "OAuth %s" %  TOKEN,
+#    'Accept':'application/json',
+    }
+
+@app.route('/')
+def hello_world():
+    #return json.dumps(getTokenGivenUsernamePasswordAndConsumerKey('user46@concurdisrupt.com', 'disrupt', KEY))
+    #Voltando um itinerario
+    resp = requests.get('https://www.concursolutions.com/api/travel/trip/v1.1',headers = auth)
+    print resp.content
+    return ElementTree.fromstring(resp.content)
+
+if __name__ == '__main__':
+    app.run(debug = True)
